1aa60dbf72c0c77dc29989605958c13ed621579e
PatrykZymelka <patrykzymelka@op.pl>
2023-08-21 19:15:31 +0200

    0.2.2
    Dodano wyszukiwarkę spelli po nazwie

--- src/pages/magia.jsx
+++ src/pages/magia.jsx
@@ -2,12 +2,14 @@ import { Component } from "react";
 import magia from "../Json/magia.json";
 import ItemList from "../components/item-list/item-list";
 import "./Pages-Styles/magia.styles.css";
+import SearchBox from "../components/search-box/search-box.component"
 
 class Magia extends Component{
     constructor(){
         super();
         this.state ={
-          Magia: []
+          Magia: [],
+          searchBar: ''
         }
     };
 
@@ -16,16 +18,38 @@ class Magia extends Component{
           Magia: magia
         })
     }
-      
+    
+    onSearchChange = (event) => 
+    {const searchBar = event.target.value.toLowerCase();
+    this.setState(
+        () => {
+        return{searchBar};
+        });
+    }
+
     render(){
-        const Mlist = this.state.Magia;
+        const {searchBar, Magia} = this.state;
+        const {onSearchChange} = this;
+
+        const fileteredMagia = Magia.filter((zaklęcie) => {
+            return zaklęcie.nazwa.toLowerCase().includes(searchBar);
+            });
+
         return(
-            <div className="Container">
-                {Mlist.map(item =>{
-                    return(
-                        <ItemList Mlist={item}/>
-                    );
-                })}
+            <div>
+                <div className="searchBoxBackground">
+                    <SearchBox
+                    onChangeHandler = {onSearchChange} 
+                    className = {'search-box'} 
+                    placeholder = {'Nazwa zaklęcia'}/>
+                </div>
+                <div className="Container">
+                    {fileteredMagia.map(item =>{
+                        return(
+                            <ItemList Mlist={item}/>
+                        );
+                    })}
+                </div>
             </div>
         )
     }
