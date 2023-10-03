04d86a6c59291b7ce1364e9f0c6e405355fbc00a
PatrykZymelka <patrykzymelka@op.pl>
2023-10-03 19:26:05 +0200

    0.3.5

--- src/components/HomeStrona/magAnimatic/magAnimatic.jsx
+++ src/components/HomeStrona/magAnimatic/magAnimatic.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import BackD from "../../../Images/Back-Death.png"
 import "./magAnimatic.styles.css"
-/* import Sun from "../../Images/Sun.PNG" */
+import BackS from "../../../Images/Back-Shadow.png"
 
 function magAnimatic() {
     return(
         <div className="ContAnim">
-            {/*   */}
+            <img src={BackD} className="Sun" alt="Mag"/>
+            <img src={BackS} className="Shad" alt="Mag"/>
         </div>
     )
 }
