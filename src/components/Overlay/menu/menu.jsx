b55a260d83e40987d51b7a8d8f427da7ce802afc
PatrykZymelka <patrykzymelka@op.pl>
2023-10-12 14:12:16 +0200

    0.3.12.1

---
+++ src/components/Overlay/menu/menu.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import "./menu.styles.css"
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+class Menu extends Component {
+  constructor(props, context) {
+    super(props, context);
+ 
+  this.state = {
+    visible: false
+  };
+}
+  render() {
+    const { visible } = this.state;
+    return (
+      <div className='all'>
+            <div id='slide_nav'>     
+                <p id="slide_nav_button" onClick={() => this.setState({visible: !visible })}>☰</p>
+            </div>
+            <div>
+                <ul className='slide_menu' style={{ display: (visible ? 'grid' : 'none') }}>
+                    <Link to="/magia" className="text">
+                        Magia
+                    </Link>
+                    <Link to="/profesje" className="text">
+                        Profesje
+                    </Link>
+                    <Link to="/talenty" className="text">
+                        Talenty
+                    </Link>
+                    <Link to="/umiejętności" className="text">
+                        Umiejętności
+                    </Link>
+                    
+                </ul>
+          </div>
+      </div>
+    );
+  }
+}
+export default Menu;
--- src/components/Overlay/page-menu/page-menu.jsx
+++ src/components/Overlay/page-menu/page-menu.jsx
@@ -3,7 +3,9 @@ import PageButtons from "./page-buttons/page-buttons";
 import "./page-menu.styles.css";
 import { Link } from "react-router-dom";
 import logo from "../../../Images/Logo.png"
-
+import Media from 'react-media';
+import React, { Fragment } from 'react';
+import Menu from "../menu/menu"
 
 
 class PageMenu extends Component{
@@ -17,11 +19,24 @@ render(){
             <Link to={"/"} id={"asdfqweqwe"}>
                 <img src={logo} alt="logo" className="Logo"/>
             </Link>
-                {PagesInfo.map(item =>{
-                    return(
-                        <PageButtons PagesInfo ={item} className="item" key={PagesInfo.id}/>
-                    );
-                })}
+            <Media queries={{
+                small: "(max-width: 800px)",
+                large: "(min-width: 801px)"
+              }}>
+                {matches => (
+                  <Fragment>
+                    {matches.small && <div className="burger"><Menu/></div>}
+                    {matches.large && 
+                        <div className="Bar">{PagesInfo.map(item =>{
+                        return(
+                            <PageButtons PagesInfo ={item} className="item" key={PagesInfo.id}/>
+                        );
+                    })}</div>}
+                  </Fragment>
+                )}
+              </Media>
+                
+            
             </nav>
           
         </div>
